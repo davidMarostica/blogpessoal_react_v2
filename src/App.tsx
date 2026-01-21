02c934ef2e9536a94f8d85a7fbe6047c5dd278ad
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-21 11:23:44 -0300

    Criando Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import Footer from "./components/footer /Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Footer from "./components/footer /Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/footer /Footer.tsx
+++ src/components/footer /Footer.tsx
@@ -9,16 +9,25 @@ function Footer() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="mt-auto flex justify-center bg-green-900 text-white py-4">
-        <div className="container flex flex-col items-center py-100">
+      <div className="mt-8 flex justify-center bg-green-900 text-white">
+        <div className="container flex flex-col items-center py-8">
           <p className="text-xl font-bold">
             Blog Pessoal Generation | Copyright: {data}
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight="bold" />
-            <InstagramLogoIcon size={48} weight="bold" />
-            <FacebookLogoIcon size={48} weight="bold" />
+            <a
+              href="https://www.linkedin.com/in/david-aparecido-da-silva"
+              target="_blank"
+            >
+              <LinkedinLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.instagram.com/seu_usuario" target="_blank">
+              <InstagramLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.facebook.com/seu_usuario" target="_blank">
+              <FacebookLogoIcon size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
