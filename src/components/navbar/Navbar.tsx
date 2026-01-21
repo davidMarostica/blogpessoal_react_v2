ea45e29798945eba3d00d64494521806910a9ac2
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-21 16:30:42 -0300

    logica de login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert("O Usuário foi desconectado com sucesso!");
+    navigate("/");
+  }
+
   return (
     <>
       <div className="w-full flex justify-center py-4 bg-green-900 text-white">
-        <div className="container flex justify-between text-lg  items-center">
+        <div className="container flex justify-between text-lg items-center">
           <Link to="/home" className="text-2xl font-bold">
             Blog Pessoal
           </Link>
           <div className="flex gap-4">
-            Postagens Temas Cadastrar tema Perfil Sair
+            <Link to="/postagens" className="hover:underline">
+              Postagens
+            </Link>
+            <Link to="/temas" className="hover:underline">
+              Temas
+            </Link>
+            <Link to="/cadastroTema" className="hover:underline">
+              Cadastrar tema
+            </Link>
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
+            <button onClick={logout} className="hover:underline">
+              Sair
+            </button>
           </div>
         </div>
       </div>
