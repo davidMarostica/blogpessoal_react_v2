ec29d75ddce26dff53fb4014c4396037fc12459a
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-21 16:57:15 -0300

    criado card Tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
 
 function Navbar() {
   const navigate = useNavigate();
+
   const { handleLogout } = useContext(AuthContext);
 
   function logout() {
@@ -14,27 +15,21 @@ function Navbar() {
 
   return (
     <>
-      <div className="w-full flex justify-center py-4 bg-green-900 text-white">
-        <div className="container flex justify-between text-lg items-center">
+      <div className="w-full bg-green-900 text-white flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
           <Link to="/home" className="text-2xl font-bold">
             Blog Pessoal
           </Link>
+
           <div className="flex gap-4">
-            <Link to="/postagens" className="hover:underline">
-              Postagens
-            </Link>
+            Postagens
             <Link to="/temas" className="hover:underline">
               Temas
             </Link>
-            <Link to="/cadastroTema" className="hover:underline">
-              Cadastrar tema
-            </Link>
-            <Link to="/perfil" className="hover:underline">
-              Perfil
-            </Link>
-            <button onClick={logout} className="hover:underline">
+            Cadastrar tema Perfil
+            <Link to="" onClick={logout} className="hover:underline">
               Sair
-            </button>
+            </Link>
           </div>
         </div>
       </div>
