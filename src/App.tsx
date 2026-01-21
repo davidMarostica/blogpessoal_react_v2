d6d1bbc8194587d7ce1836cfd53a318b8b159c45
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-21 11:39:40 -0300

    Criando pagina de  Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
 import Footer from "./components/footer /Footer";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <div className="min-h-[80vh]">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
